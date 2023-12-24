2d4a630938c1d4aa8cc96304f76c93c996ad039a
Ria River <riria.river@gmail.com>
2023-12-24 18:34:15 +0300

    feat: implement 'Queue' class

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  #head = null;
+
+  #tail = null;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    if (this.#tail) {
+      this.#tail.next = node;
+    } else this.#head = node;
+
+    this.#tail = node;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const removed = this.#head;
+
+    if (removed) {
+      this.#head = removed.next;
+    }
+
+    return removed?.value || null;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
